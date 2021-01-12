5b45bc45814f55dca8e6ec461261c47806ec6aea
Shee Yeow Choo <sheeyeowchoo@gmail.com>
2021-01-11 19:26:29 -0700

    print source code from raw URL

--- script.py
+++ script.py
@@ -3,4 +3,10 @@ print(requests.__version__)
 
 url = "http://www.google.com"
 res = requests.get(url)
-print(res.status_code)
+r = requests.get(url)
+print(r.status_code)
+
+
+url = "https://raw.githubusercontent.com/Jeremy0818/CMPUT404-Lab1/master/script.py"
+r = requests.get(url)
+print(r.text)
